28da3940c8c9234176971a3d254dd810b9ee0712
Nebojsa.Vuksic <nebojsa92vuksic@gmail.com>
2024-04-06 22:35:26 +0200

    add solutions for exercise 3

--- src/main/kotlin/exercise3/task2/ParenthesesClusters.kt
+++ src/main/kotlin/exercise3/task2/ParenthesesClusters.kt
@@ -23,14 +23,122 @@ package exercise3.task2
  *
  */
 
+private sealed class ClusterProcessing {
+    abstract val clusters: List<String>
+
+    abstract fun process(bracketCharacter: Char): ClusterProcessing
+
+    data object InvalidCluster : ClusterProcessing() {
+        override val clusters: List<String> = emptyList()
+        override fun process(bracketCharacter: Char): ClusterProcessing = InvalidCluster
+    }
+
+    data class ValidCluster(
+        private val accumulatorValue: String = "",
+        private val openBracketsCounter: OpenBracketsCounter = OpenBracketsCounter(),
+        private val totalCharacters: Int = 0
+    ) : ClusterProcessing() {
+        private val clusterEndChar = "|"
+        private val closedToOpenBrackets = mapOf(']' to '[', '}' to '{', ')' to '(')
+        private val closeBrackets = closedToOpenBrackets.keys
+        private val openBrackets = closedToOpenBrackets.values
+
+        override val clusters: List<String>
+            // If there is any open brackets expression is invalid, hence no clusters should be returned.
+            get() = if (openBracketsCounter.anyOpen) {
+                emptyList()
+            } else {
+                accumulatorValue.split(clusterEndChar).filter(String::isNotBlank)
+            }
+
+        override fun process(bracketCharacter: Char): ClusterProcessing {
+            return when {
+                // Case when we open the bracket
+                bracketCharacter in openBrackets -> {
+                    ValidCluster(accumulatorValue + bracketCharacter, openBracketsCounter.increment(bracketCharacter))
+                }
+                // We add close bracket when there previously was no open bracket
+                bracketCharacter in closeBrackets
+                        && openBracketsCounter[closedToOpenBrackets.getValue(bracketCharacter)] == 0 -> InvalidCluster
+                // Case when we close the bracket
+                else -> {
+                    val openBracket = closedToOpenBrackets.getValue(bracketCharacter)
+                    val currentOpenBracketCount = openBracketsCounter[openBracket]
+                    // Case when we close the bracket but there is more open brackets pending
+                    if (accumulatorValue.lastOrNull() == openBracket || currentOpenBracketCount > 0) {
+                        val newOpenBracketsCounter = openBracketsCounter.decrement(bracketCharacter)
+                        val accumulatorValue = buildString {
+                            append(accumulatorValue)
+                            append(bracketCharacter)
+                            if (newOpenBracketsCounter[openBracket] == 0) append(clusterEndChar)
+                        }
+
+                        ValidCluster(accumulatorValue, newOpenBracketsCounter)
+                    } else {
+                        // Case when we add close the bracket but there is no more open brackets to close
+                        InvalidCluster
+                    }
+                }
+            }
+        }
+
+        /**
+         * Classed used to track opened brackets
+         */
+        data class OpenBracketsCounter(
+            // Corresponds to `(`
+            private val roundBracketCounter: Int = 0,
+            // Corresponds to `{`
+            private val curlyBracketCounter: Int = 0,
+            // Corresponds to `[`
+            private val squareBracketCounter: Int = 0
+        ) {
+            val anyOpen: Boolean get() = roundBracketCounter != 0 || curlyBracketCounter != 0 || squareBracketCounter != 0
+
+            fun increment(bracket: Char): OpenBracketsCounter {
+                return when (bracket) {
+                    '(' -> this.copy(roundBracketCounter = roundBracketCounter + 1)
+                    '{' -> this.copy(curlyBracketCounter = curlyBracketCounter + 1)
+                    '[' -> this.copy(squareBracketCounter = squareBracketCounter + 1)
+                    else -> error("Unknown bracket character: $bracket")
+                }
+            }
+
+            fun decrement(bracket: Char): OpenBracketsCounter {
+                return when (bracket) {
+                    ')' -> this.copy(roundBracketCounter = (roundBracketCounter - 1).coerceAtLeast(0))
+                    '}' -> this.copy(curlyBracketCounter = (curlyBracketCounter - 1).coerceAtLeast(0))
+                    ']' -> this.copy(squareBracketCounter = (squareBracketCounter - 1).coerceAtLeast(0))
+                    else -> error("Unknown bracket character: $bracket")
+                }
+            }
+
+            operator fun get(bracketChar: Char): Int {
+                return when (bracketChar) {
+                    '(' -> roundBracketCounter
+                    '{' -> curlyBracketCounter
+                    '[' -> squareBracketCounter
+                    else -> error("Unknown bracket character: $bracketChar")
+                }
+            }
+        }
+    }
+}
+
 internal fun String.splitToBracketsClusters(): List<String> {
-    TODO("Implement me!!!")
+    val initial = ClusterProcessing.ValidCluster()
+
+    return this.fold<ClusterProcessing>(initial) { clusterResult, bracketCharacter ->
+        clusterResult.process(bracketCharacter)
+    }.clusters
 }
 
 fun main() {
     val expressionsToClustersCatalog = mapOf(
         "()()()" to listOf("()", "()", "()"),
         "((()))" to listOf("((()))"),
+        "())" to emptyList(),
+        "()(" to emptyList(),
         "((()))(())()()(()())" to listOf("((()))", "(())", "()", "()", "(()())"),
         "((())())(()(()()))" to listOf("((())())", "(()(()()))")
     )
--- src/main/kotlin/exercise3/task4/TaxiParkTask.kt
+++ src/main/kotlin/exercise3/task4/TaxiParkTask.kt
@@ -9,30 +9,50 @@ package exercise3.task4
  * Subtask 1:
  * Find all the drivers who performed no trips.
  */
-internal fun TaxiPark.findFakeDrivers(): Set<Driver> {
-    TODO("Implement me!!!")
-}
+internal fun TaxiPark.findFakeDrivers(): Set<Driver> = allDrivers
+    .subtract(trips.map { it.driver }.toSet())
 
 /**
  * Subtask 2:
  * Find all the clients who completed at least the given number of trips.
  */
-internal fun TaxiPark.findFaithfulPassengers(minTrips: Int): Set<Passenger> {
-    TODO("Implement me!!!")
-}
+internal fun TaxiPark.findFaithfulPassengers(minTrips: Int): Set<Passenger> =
+    trips
+        .flatMap { it.passengers }
+        .groupingBy { passenger -> passenger }
+        .eachCount()
+        .filter { (_, numOfTrips) -> numOfTrips >= minTrips }
+        .map { it.key }
+        .toSet()
 
 /**
  * Subtask 3:
  * Find all the passengers, who were taken by a given driver more than once.
  */
-internal fun TaxiPark.findFrequentPassengers(driver: Driver): Set<Passenger> {
-    TODO("Implement me!!!")
-}
+internal fun TaxiPark.findFrequentPassengers(driver: Driver): Set<Passenger> = trips
+    .filter { trip -> trip.driver == driver }
+    .flatMap { trip -> trip.passengers }
+    .groupingBy { passenger -> passenger }
+    .eachCount()
+    .filter { (_, numOfTrips) -> numOfTrips > 1 }
+    .keys
 
 /**
  * Subtask 4:
  * Find the passengers who had a discount for the majority of their trips.
  */
 internal fun TaxiPark.findSmartPassengers(): Set<Passenger> {
-    TODO("Implement me!!!")
+    return trips
+        .flatMap { trip ->
+            val discountCount = if (trip.discount == null) 0 else 1
+            trip.passengers.map { passenger -> passenger to discountCount }
+        }.fold(HashMap<Passenger, Int>()) { passengerToDiscountCountMap, (passenger, discountCount) ->
+            // We always read current num of discounts and update it with a discount count
+            //  If a passenger is not already added to the map, we add it, and then we increment discount count value
+            val currentDiscountCount = passengerToDiscountCountMap[passenger] ?: 0
+            passengerToDiscountCountMap[passenger] = currentDiscountCount + discountCount
+
+            passengerToDiscountCountMap
+        }.filter { it.value > 0 }
+        .keys
 }
