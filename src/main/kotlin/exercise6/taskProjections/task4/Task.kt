0c9cfc0ff957a833abb9388ab1500fd183e84b64
Miša Stefanović <mikmikyt1011@gmail.com>
2024-04-28 14:12:26 +0200

    add solutions for exercise 6

--- src/main/kotlin/exercise6/taskProjections/task4/Task.kt
+++ src/main/kotlin/exercise6/taskProjections/task4/Task.kt
@@ -7,6 +7,15 @@ package exercise6.taskProjections.task4
  * that accepts two arrays with a generic type of Any and copy elements from the first array to the second one.
 **/
 
+fun <Any> copy(arr1 : Array<Any>, arr2 : Array<in Any>) {
+    require(arr2.size >= arr1.size) {
+        throw Exception("Second array is shorter than first!")
+    }
+
+    for (i in arr1.indices)
+            arr2[i] = arr1[i]
+}
+
 fun main() {
     val ints = arrayOf(1, 2, 3)
     val any = Array<Any>(3) { "" }
@@ -17,8 +26,7 @@ fun main() {
     println("_______")
     println("_______")
 
-    // TODO: uncomment this code
-   // copy(ints, any)
+    copy(ints, any)
 
     ints.forEach{ print("$it ") }
     println()
