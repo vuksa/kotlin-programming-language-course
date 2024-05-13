c95e5ea0a7f61486d176fed5e3e6a413c58166b8
name <you@example.com>
2024-05-13 18:34:58 +0200

    Movies almost done

--- src/main/kotlin/exercise8/MovieDB.kt
+++ src/main/kotlin/exercise8/MovieDB.kt
@@ -8,3 +8,57 @@ package exercise8
  * Implement methods defined by MovieDBApi.
  *
  */
+
+class MovieDB(
+    private val movies: List<Movie>
+): MovieDBApi {
+    override fun getAllMoviesByActor(actor: MovieActor): List<Movie> {
+        return movies.filter { movie -> movie.actors.contains(actor) }
+    }
+
+    override fun getMoviesWithBiggestProfit(numOfMovies: Int): List<Movie> {
+        return movies.sortedBy { it.revenue - it.budget }
+            .take(numOfMovies)
+    }
+
+    override fun getBestRatedMovieByActor(actor: MovieActor): Movie? {
+        return movies.filter { movie -> movie.actors.contains(actor) }
+            .maxBy { it.rating }
+    }
+
+    override fun getAllMoviesByYear(year: Int): List<Movie> {
+        return movies.filter { movie -> movie.releaseDate.year == year }
+    }
+
+    override fun getAllMoviesByGenre(genre: String): List<Movie> {
+        return movies.filter { movie -> movie.genres.contains(genre) }
+    }
+
+    override fun getBestRatedMovies(numOfMovies: Int): List<Movie> {
+        return movies.sortedBy { it.rating }
+            .take(numOfMovies)
+    }
+
+    override fun getDirectorWithMostMoviesDirected(): MovieDirector {
+        return movies.groupingBy { it.director }
+            .eachCount()
+            .maxBy { it.value }
+            .key
+    }
+
+    override fun getActorsWithMostCostarredMovies(): List<Pair<MovieActor, MovieActor>> {
+        TODO("IMPL")
+
+
+//        return movies.groupingBy { Pair(it.actors, it.actors) }
+//            .eachCount()
+//            .maxBy { it.value }
+//
+//        val appearances = movies.groupingBy { it.actors }
+
+
+
+    }
+
+
+}
--- src/main/kotlin/exercise8/MovieDBRunner.kt
+++ src/main/kotlin/exercise8/MovieDBRunner.kt
@@ -1,15 +1,44 @@
 package exercise8
 
 import common.FileReader
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 
 private fun parseMovies(moviesLines: List<String>): List<Movie> {
-    TODO("Implement parsing of the file content")
+    val movies : MutableList<Movie> = emptyList<Movie>().toMutableList()
+    for(movie in moviesLines.drop(1)) {
+        val actors : MutableList<MovieActor> = emptyList<MovieActor>().toMutableList()
+        val line = movie.split(";")
+        val budget = line[0].trim().toLong()
+        val genres = line[1].split(",")
+        val title = line[2].trim()
+        val rating = line[3].trim().toDouble()
+        val releaseDate = LocalDate.parse(line[4])
+        val revenue = line[5].trim().toLong()
+        val runtime = line[6].trim().toDouble().toInt()
+        val cast = line[7].split(",")
+        for(actor in cast) {
+            actors.add(MovieActor(actor.trim()))
+        }
+        val director = MovieDirector(line[8])
+
+        movies.add(Movie(title, budget, revenue, releaseDate, runtime, director, genres, actors, rating))
+    }
+    return movies
 }
 
 fun main() {
     val moviesCSVFile = FileReader.readFileInResources("exercise8/movies.csv")
     val movies = parseMovies(moviesCSVFile)
 
-    val movieDBApi : MovieDBApi = TODO("Instantiate MovieDB")
+    val movieDBApi : MovieDBApi = MovieDB(movies)
+    //println(movieDBApi.getAllMoviesByActor(MovieActor("Johnny Depp")))
+    //println(movieDBApi.getMoviesWithBiggestProfit(2))
+    //println(movieDBApi.getBestRatedMovieByActor(MovieActor("Johnny Depp")))
+    //println(movieDBApi.getAllMoviesByYear(2003))
+    //println(movieDBApi.getDirectorWithMostMoviesDirected())
+
+
+
 }
